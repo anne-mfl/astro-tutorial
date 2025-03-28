6eb477b5de46ad2bff89d0dfa18fd777c29c1b68
anne <anne.gemma.lee@gmail.com>
2025-03-28 12:00:52 +0100

    Add Astro Island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://app.netlify.com/sites/famous-axolotl-6f5389/overview"
+  site: "https://app.netlify.com/sites/famous-axolotl-6f5389/overview",
+  integrations: [preact()]
 });
